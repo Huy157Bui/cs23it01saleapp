4a9365dc894ed4553de9fce6d653f6a52eba6d17
Huy157Bui <162878885+Huy157Bui@users.noreply.github.com>
2025-12-06 11:21:50 +0700

    update

--- saleappv2/eapp/dao.py
+++ saleappv2/eapp/dao.py
@@ -1,8 +1,13 @@
-from cs23it01saleapp.saleappv2.eapp.models import Category, Product
+from saleappv2.eapp.models import Category, Product, User
+from saleappv2.eapp import app, db
+import hashlib
+import cloudinary.uploader
+
 
 def get_categories():
     return Category.query.all()
-def get_products(kw=None, category_id=None):
+
+def get_products(kw=None, category_id=None, page=1):
     products = Product.query
 
     if category_id:
@@ -11,4 +16,37 @@ def get_products(kw=None, category_id=None):
     if kw:
         products = products.filter(Product.name.contains(kw))
 
+    if page:
+        page = int(page)
+        page_size =  app.config.get('PAGE_SIZE', 6)
+        start = (page - 1) * page_size
+        products = products.slice(start, start + page_size)
+
     return products.all()
+
+
+def count_products():
+    return Product.query.count()
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    return User.query.filter(User.username==username.strip(),
+                             User.password==password).first()
+
+
+def add_user(name, username, password, avatar):
+    u = User(name=name,
+             username=username.strip(),
+             password=str(hashlib.md5(password.strip().encode('utf-8')).hexdigest()))
+
+    if avatar:
+        res = cloudinary.uploader.upload(avatar)
+        u.avatar = res.get('secure_url')
+
+    db.session.add(u)
+    db.session.commit()
--- saleappv2/eapp/models.py
+++ saleappv2/eapp/models.py
@@ -1,13 +1,32 @@
-from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey
-from sympy import false
-from unicodedata import category
+from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
-from cs23it01saleapp.saleappv2.eapp import app, db
+from flask_login import UserMixin
+from enum import Enum as UserEnum
+
+from saleappv2.eapp import db, app
+
 
 class BaseModel(db.Model):
     __abstract__ = True
+
     id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class UserRole(UserEnum):
+    USER = 1
+    ADMIN = 2
+
+
+class User(BaseModel, UserMixin):
+    name = Column(String(50), nullable=False)
+    avatar = Column(String(100))
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    user_role = Column(Enum(UserRole), default=UserRole.USER)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(BaseModel):
@@ -22,21 +41,32 @@ class Product(BaseModel):
     name = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
     price = Column(Float, default=0)
-    image = Column(String(100), default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg")
+    image = Column(String(100),
+                   default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg')
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
     def __str__(self):
-        return str.subject
+        return self.name
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # categories = ['Điện thoại', 'Máy tính bảng', 'Máy tính xách tay']
-        # for cate in categories:
-        #     c = Category(name=cate)
-        #     db.session.add(c)
-        # db.session.commit()
+
+        import hashlib
+
+        u = User(name='admin', user_role=UserRole.ADMIN,
+                 avatar='https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg',
+                 username='admin', password=str(hashlib.md5("123456".encode('utf-8')).hexdigest()))
+        db.session.add(u)
+        db.session.commit()
+
+        categories = ['Điện thoại', 'Máy tính bảng', 'Máy tính xách tay']
+        for cate in categories:
+            c = Category(name=cate)
+            db.session.add(c)
+        db.session.commit()
+
         products = [{
             "name": "iPhone 7 Plus",
             "description": "Apple, 32GB, RAM: 3GB, iOS13",
@@ -53,7 +83,7 @@ if __name__=='__main__':
             "category_id": 2
         }, {
             "name": "Galaxy Note 10 Plus",
-            "description": "Samsung, 64GB, RAML: 6GB",
+            "description": "Samsung, 64GB, RAM: 6GB",
             "price": 24000000,
             "image":
                 "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
@@ -103,6 +133,6 @@ if __name__=='__main__':
         }]
 
         for p in products:
-            o=Product(**p)
+            o = Product(**p)
             db.session.add(o)
         db.session.commit()
